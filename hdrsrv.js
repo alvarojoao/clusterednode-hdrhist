ccff4dc6a5e4d1ad1d19f41f6dbd5eec2a66057c
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-21 10:39:46 -0300

    Fixed precision points for HDR histogram sampling

--- hdrsrv.js
+++ hdrsrv.js
@@ -43,85 +43,20 @@ var server = http2.createServer({
                 {"percentile": 99.21875, "value": 0},
                 {"percentile": 100, "value": 0}
             ];
-            var results2 = [
-                {"percentile": 1, "value": 0},
-                {"percentile": 5, "value": 0},
-                {"percentile": 10, "value": 0},
-                {"percentile": 15, "value": 0},
-                {"percentile": 20, "value": 0},
-                {"percentile": 25, "value": 0},
-                {"percentile": 30, "value": 0},
-                {"percentile": 35, "value": 0},
-                {"percentile": 40, "value": 0},
-                {"percentile": 45, "value": 0},
-                {"percentile": 50, "value": 0},
-                {"percentile": 55, "value": 0},
-                {"percentile": 60, "value": 0},
-                {"percentile": 65, "value": 0},
-                {"percentile": 70, "value": 0},
-                {"percentile": 75, "value": 0},
-                {"percentile": 76, "value": 0},
-                {"percentile": 77, "value": 0},
-                {"percentile": 78, "value": 0},
-                {"percentile": 79, "value": 0},
-                {"percentile": 80, "value": 0},
-                {"percentile": 81, "value": 0},
-                {"percentile": 82, "value": 0},
-                {"percentile": 83, "value": 0},
-                {"percentile": 84, "value": 0},
-                {"percentile": 85, "value": 0},
-                {"percentile": 86, "value": 0},
-                {"percentile": 87, "value": 0},
-                {"percentile": 88, "value": 0},
-                {"percentile": 89, "value": 0},
-                {"percentile": 90, "value": 0},
-                {"percentile": 91, "value": 0},
-                {"percentile": 92, "value": 0},
-                {"percentile": 93, "value": 0},
-                {"percentile": 94, "value": 0},
-                {"percentile": 95, "value": 0},
-                {"percentile": 96, "value": 0},
-                {"percentile": 97, "value": 0},
-                {"percentile": 98, "value": 0},
-                {"percentile": 98.1, "value": 0},
-                {"percentile": 98.2, "value": 0},
-                {"percentile": 98.3, "value": 0},
-                {"percentile": 98.4, "value": 0},
-                {"percentile": 98.5, "value": 0},
-                {"percentile": 98.6, "value": 0},
-                {"percentile": 98.7, "value": 0},
-                {"percentile": 98.8, "value": 0},
-                {"percentile": 98.9, "value": 0},
-                {"percentile": 99, "value": 0},
-                {"percentile": 99.1, "value": 0},
-                {"percentile": 99.2, "value": 0},
-                {"percentile": 99.3, "value": 0},
-                {"percentile": 99.4, "value": 0},
-                {"percentile": 99.5, "value": 0},
-                {"percentile": 99.6, "value": 0},
-                {"percentile": 99.7, "value": 0},
-                {"percentile": 99.8, "value": 0},
-                {"percentile": 99.9, "value": 0},
-                {"percentile": 99.91, "value": 0},
-                {"percentile": 99.92, "value": 0},
-                {"percentile": 99.93, "value": 0},
-                {"percentile": 99.94, "value": 0},
-                {"percentile": 99.95, "value": 0},
-                {"percentile": 99.96, "value": 0},
-                {"percentile": 99.97, "value": 0},
-                {"percentile": 99.98, "value": 0},
-                {"percentile": 99.99, "value": 0},
-                {"percentile": 99.991, "value": 0},
-                {"percentile": 99.992, "value": 0},
-                {"percentile": 99.993, "value": 0},
-                {"percentile": 99.994, "value": 0},
-                {"percentile": 99.995, "value": 0},
-                {"percentile": 99.996, "value": 0},
-                {"percentile": 99.997, "value": 0},
-                {"percentile": 99.998, "value": 0},
-                {"percentile": 99.999, "value": 0},
-                {"percentile": 100, "value": 0}
-            ];
+            var results2 = [];
+            for (i = 1; i < 90; i++) {
+                results2.push({"percentile": i, "value": 0});
+            }
+            for (i = 90; i < 99.9; i += 0.1) {
+                results2.push({"percentile": i, "value": 0});
+            }
+            for (i = 99.9; i < 99.99; i += 0.01) {
+                results2.push({"percentile": i, "value": 0});
+            }
+            for (i = 99.99; i < 99.999; i += 0.001) {
+                results2.push({"percentile": i, "value": 0});
+            }
+            results2.push({"percentile": 100, "value": 0});
             try {
                 histogram = new hdr(min, max, 5);
                 for (i = 0; i < data.arr.length; i++)
